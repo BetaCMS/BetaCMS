3d2f3f5d8052ce23fe6f24dcea7f92415825a8fd
hizhengfu <hizhengfu@gmail.com>
2013-12-30 23:29:40 +0800

    引入自动创建表函数

--- app/models/User.php
+++ app/models/User.php
@@ -8,24 +8,22 @@ class User extends Model
 
     public function index()
     {
-        $user=new DB\SQL\Mapper($this->db,'users',array(
-            'order',
-            'group',
-            'limit',
-            'offset'
-        ));
-
-        $user->save();
-
-        //$user->insert('admin','admin888');
+        $schema = new \DB\SQL\Schema($this->db);
 
 
-        echo $this->db->version();
-        echo '\n';
-        echo $this->db->name();
-        echo '\n';
-        $columns=$this->db->schema('mytable');
-        var_dump($columns);
+        $table = $schema->createTable($this->app->get('db.prefix') . '_users');
+        $table->addColumn('ID')->type($schema::DT_INT1)->nullable(false);
+        $table->addColumn('user_login')->type($schema->type_varchar(60))->defaults('')->nullable(false);
+        $table->addColumn('user_pass')->type($schema->type_varchar(64))->defaults('')->nullable(false);
+        $table->addColumn('user_nicename')->type($schema->type_varchar(50))->defaults('')->nullable(false);
+        $table->addColumn('user_email')->type($schema->type_varchar(100))->defaults('')->nullable(false);
+        $table->addColumn('user_url')->type($schema->type_varchar(100))->defaults('')->nullable(false);
+        $table->addColumn('user_registered')->type($schema::DT_DATETIME)->defaults('0000-00-00 00:00:00')->nullable(false);;
+        $table->addColumn('user_activation_key')->type($schema->type_varchar(60))->defaults('');
+        $table->addColumn('user_status')->type($schema::DT_INT)->defaults('0')->nullable(false);;
+        $table->addColumn('display_name')->type($schema->type_varchar(250))->defaults('')->nullable(false);;
+        $table->build();
+
 
         echo 'models';
     }
